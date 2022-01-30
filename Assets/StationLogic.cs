da154f0f495a13edcc6bf0476c41f457a51d9466
stekra <stekra@me.com>
2022-01-30 01:17:44 +0100

    Station work

---
+++ Assets/StationLogic.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationLogic : MonoBehaviour
+{
+    public bool active = false;
+    List<StationLogic> otherStations;
+
+    public GameObject activeVisuals;
+    public GameObject inactiveVisuals;
+
+    void Start()
+    {
+        otherStations = new List<StationLogic>();
+        otherStations.AddRange(FindObjectsOfType<StationLogic>());
+        otherStations.Remove(this);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (active)
+        {
+            active = false;
+            SetVisuals();
+            StartCoroutine(ChooseNewStation());
+        }
+
+        print("yo");
+    }
+
+    public void SetVisuals()
+    {
+        activeVisuals.SetActive(active);
+        inactiveVisuals.SetActive(!active);
+    }
+
+    IEnumerator ChooseNewStation()
+    {
+        yield return new WaitForSeconds(2);
+        int choice = Random.Range(0, otherStations.Count);
+        otherStations[choice].active = true;
+        otherStations[choice].SetVisuals();
+    }
+}
